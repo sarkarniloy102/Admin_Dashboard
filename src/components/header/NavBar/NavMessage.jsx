f8a799200f429aa53f5136d02d432c5b8eec363b
sarkar niloy <sarkarniloy102@gmail.com>
2025-09-01 23:47:07 +0600

    Messages section in navbar

--- src/components/header/NavBar/NavMessage.jsx
+++ src/components/header/NavBar/NavMessage.jsx
@@ -1,10 +1,81 @@
-
+import './nav.css';
 
 const NavMessage = () => {
     return (
-        <div clasName="">
-            <h2>Message page</h2>
-        </div>
+        <li className='nav-item dropdown'>
+            <a href="#"
+                className='nav-link nav-icon'
+                data-bs-toggle="dropdown">
+                <i className='bi bi-chat-left-text'></i>
+                <span className='badge bg-success badge-number'>3</span>
+            </a>
+            <ul className="dropdown-menu dropdown-menu-end dropdown-menu-arrow messages">
+                <li className='dropdown-header'>
+                    You Have 3 new messages
+                    <a href="#"
+                    >
+                        <span className='badge rounded-pill bg-primary p-2 ms-2'>view all</span>
+                    </a>
+                </li>
+                <li>
+                    <hr className='dropdown-divider' />
+                </li>
+                <li className='messages-item'>
+                    <a href="#">
+                        <img src="/src/assets/messages.png" alt="" className="rounded-circle" />
+                    </a>
+                    <div>
+                        <h4>Lorem ipsum </h4>
+                        <p>amet consectetur adipisicing elit. </p>
+                        <p>30min ago</p>
+                    </div>
+                </li>
+                <li>
+                    <hr className='dropdown-divider' />
+                </li>
+                <li className='messages-item'>
+                    <a href="#">
+                        <img src="/src/assets/messages.png" alt="" className="rounded-circle" />
+                    </a>
+                    <div>
+                        <h4>Lorem ipsum </h4>
+                        <p>amet consectetur adipisicing elit. </p>
+                        <p>30min ago</p>
+                    </div>
+                </li>
+                <li>
+                    <hr className='dropdown-divider' />
+                </li>
+                <li className='messages-item'>
+                    <a href="#">
+                        <img src="/src/assets/messages.png" alt="" className="rounded-circle" />
+                    </a>
+                    <div>
+                        <h4>Lorem ipsum </h4>
+                        <p>amet consectetur adipisicing elit. </p>
+                        <p>30min ago</p>
+                    </div>
+                </li>
+                <li>
+                    <hr className='dropdown-divider' />
+                </li>
+                {/* <li className='notifications-item'>
+                    <i className='bi bi-info-circle text-primary'></i>
+                    <div>
+                        <h4>Dicta </h4>
+                        <p> Quae dolorem earum veritatis oditseno  </p>
+                        <p>4 hr. ago</p>
+                    </div>
+                </li>
+                <li>
+                    <hr className='dropdown-divider' />
+                </li> */}
+                <li className='dropdown-footer'>
+                    <a href="#">Show all notifications</a>
+                </li>
+            </ul>
+
+        </li>
     );
 };
 
